ca5fb554b18b0b6eb391f1e03ada4ce8ce2703a3
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-10-19 17:32:21 -0400

    Handle args

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,17 @@ use std::process::Command;
 fn main() {
     loop {
         print!("> ");
+        // need to explicitly flush this to ensure it prints before read_line
         io::stdout().flush().unwrap();
-
+        
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-
-        let command = input.trim();
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
         let mut child = Command::new(command)
+            .args(args)
             .spawn()
             .unwrap();
 
